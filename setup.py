925cb30e778d7f0fa1b433fc7be2e291103e70f1
milaq <micha.laqua@gmail.com>
2019-07-10 14:58:14 +0200

    more python packaging preparations

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ycast',
+    version='1.0.0',
+    author='Micha LaQua',
+    author_email='micha.laqua@gmail.com',
+    description='Self hosted vTuner internet radio service emulation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/milaq/YCast',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords=[
+        'ycast',
+        'vtuner',
+        'internet radio',
+        'shoutcast',
+        'avr',
+        'emulation',
+        'yamaha',
+        'onkyo',
+        'denon'
+    ],
+    install_requires=['flask', 'PyYAML'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests'])
+)
